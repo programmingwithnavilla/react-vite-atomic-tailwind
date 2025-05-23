0ec7a9c97f875414bdd5643fcc3afddc6b1765f9
programmingwithnavilla <navidbarsalari@gmail.com>
2025-05-23 15:49:32 +0330

    add auth template

---
+++ src/components/templates/auth-template/auth-template.template.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AuthForm from "../../organisms/auth-form/auth-form.organism";
+
+export const AuthTemplate: React.FC = () => (
+  <div className="min-h-screen flex items-center justify-center">
+    <AuthForm />
+  </div>
+);

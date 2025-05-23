5f0a119f13452fa9eff3a14bc24da3ec3a051c46
programmingwithnavilla <navidbarsalari@gmail.com>
2025-05-23 15:29:07 +0330

    fix taildwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-import type { Config } from "tailwindcss";
-const config: Config = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-export default config;

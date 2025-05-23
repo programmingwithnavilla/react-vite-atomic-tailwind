2c27f46c2220305db7940e60d3a692165d60575d
programmingwithnavilla <navidbarsalari@gmail.com>
2025-05-23 15:50:55 +0330

    use template

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "./components/atoms";
+import { AuthTemplate } from "./components/templates/auth-template/auth-template.template";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          className="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-md transition duration-300"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">
+        Atomic Design with Vite + React + TS
+      </h1>
+      <Button label="Click Me" onClick={() => alert("Clicked!")} />
+      <AuthTemplate />
+    </div>
   );
 }
 

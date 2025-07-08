520f035c22b7d201e0755b3226bcfbfee9996326
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-08 23:37:07 +0330

    add rest to label

--- src/components/atoms/label/label.atom.tsx
+++ src/components/atoms/label/label.atom.tsx
@@ -5,10 +5,12 @@ export const Label: React.FC<LabelProps> = ({
   htmlFor,
   children,
   className = "",
+  ...rest
 }) => (
   <label
     htmlFor={htmlFor}
     className={`block mb-1 text-sm font-medium text-gray-700 ${className}`}
+    {...rest}
   >
     {children}
   </label>

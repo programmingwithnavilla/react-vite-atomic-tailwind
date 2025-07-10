586e3843faa29f4266bbe6386ed745d536a42f79
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-10 23:54:36 +0330

    add simple store

---
+++ src/store/store.ts
@@ -0,0 +1,10 @@
+import { createStore } from 'jotai';
+
+export const jotaiStore = createStore();
+
+// optional: set initial values
+
+// jotaiStore.set(userAtom, {
+//   name: "Guest",
+//   role: "user",
+// });

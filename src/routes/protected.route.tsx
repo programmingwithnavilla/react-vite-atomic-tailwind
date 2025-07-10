32a4381d1fbc9f0f23cc7e4624e68b61d6ab8914
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-10 23:33:09 +0330

    add new address of useAuth

--- src/routes/protected.route.tsx
+++ src/routes/protected.route.tsx
@@ -1,6 +1,6 @@
-import React, { type JSX } from "react";
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import React, { type JSX } from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/use-auth/use-auth.hook';
 
 export const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({
   children,
